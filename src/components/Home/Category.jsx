deeadaeda486905cbd9e7273ccee8c49df04ee79
Awashek Acharya <awashek.acharya16@gmail.com>
2024-10-08 10:49:44 +0545

    Caregory and Card component

--- src/components/Home/Category.jsx
+++ src/components/Home/Category.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export default function Category() {
     return (
         <div>
-            <h1 className='p-4 text-4xl text-center font-semibold'>Findraising Categories</h1>
+            <h1 className='p-4 text-4xl text-center font-semibold'>Fundraising Categories</h1>
             <div className='flex flex-row justify-center'>
-            <button className='btns'>Medical</button>
+            <button className='btns bg-red-300'>Medical</button>
             <button className='btns'>Personal</button>
             <button className='btns'>Campaign</button>
             <button className='btns'>Animals</button>
